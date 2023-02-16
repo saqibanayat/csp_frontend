59da3477a15b0b096b2b09ce724834a5f579e9f7
Rana Hussain Sakhawat <ranahussain621@gmail.com>
2023-02-16 11:30:14 +0500

    axios updated

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // {For simple request to backend server}
 export default axios.create({
-    baseURL:'http://localhost:8000',
+    baseURL:'http://192.168.0.121:8000',
     withCredentials:true,
     headers:
     {
@@ -15,7 +15,7 @@ export default axios.create({
 // {For admin request to backend server including Token }
 
 export const axiosPrivate = axios.create({
-    baseURL:'http://localhost:8000',
+    baseURL:'http://192.168.0.121:8000',
     withCredentials:true,
     headers:
     {
